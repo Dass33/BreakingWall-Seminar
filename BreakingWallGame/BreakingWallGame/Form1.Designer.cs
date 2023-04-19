971b64067601cf6449f3e40d7c3647a3ee0abd92
Dass33 <94531241+Dass33@users.noreply.github.com>
2023-04-19 09:54:24 +0200

    Add files via upload

---
+++ BreakingWallGame/BreakingWallGame/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace BreakingWallGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbplatno = new System.Windows.Forms.PictureBox();
+            this.tmrRedraw = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbplatno)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbplatno
+            // 
+            this.pbplatno.Location = new System.Drawing.Point(65, 40);
+            this.pbplatno.Name = "pbplatno";
+            this.pbplatno.Size = new System.Drawing.Size(600, 600);
+            this.pbplatno.TabIndex = 0;
+            this.pbplatno.TabStop = false;
+            // 
+            // tmrRedraw
+            // 
+            this.tmrRedraw.Interval = 10;
+            this.tmrRedraw.Tick += new System.EventHandler(this.tmrRedraw_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(753, 686);
+            this.Controls.Add(this.pbplatno);
+            this.Name = "Form1";
+            this.Text = "Breaking Wall";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbplatno)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbplatno;
+        private System.Windows.Forms.Timer tmrRedraw;
+    }
+}
+
---
+++ BreakingWallGame/BreakingWallGame/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BreakingWallGame
+{
+    public partial class Form1 : Form
+    {
+
+        //grafika pro krelsleni
+        Graphics mobjGrafika;
+
+        //trida kulicky
+        //objekt vytvoreny z tridy kulicka
+        clsKulicka mobjKulicka;
+
+        //trida pro cihly
+        int mintPocetCihel;
+        clsCihla[] mobjCihla;
+
+        //timer
+        const int tmrRedrawSpeed = 20;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //init Timeru 
+            tmrRedraw.Interval = tmrRedrawSpeed;
+            tmrRedraw.Start();
+        }
+
+        //konstruktor se vola pred loadem
+        public Form1()
+        {
+            InitializeComponent();
+
+            //init promenych
+            mobjGrafika = pbplatno.CreateGraphics();
+
+            //vytvoreni kulicky
+            mobjKulicka = new clsKulicka(mobjGrafika);
+
+            //vytvoreni pole
+            mintPocetCihel = 10;
+            mobjCihla = new clsCihla[mintPocetCihel];
+
+            //vytvoreni cihel
+            for (int i=0; i<mintPocetCihel; i++)
+            {
+                //vytvoreni jednitlivych cihel
+                mobjCihla[i] = new clsCihla();
+            }
+        }
+
+        private void tmrRedraw_Tick(object sender, EventArgs e)
+        {
+            //smazat scenu
+            mobjGrafika.Clear(Color.White);
+
+            //posun kulicky
+            mobjKulicka.Pohyb();
+        }
+    }
+}
